6023ecda07be43eb513a7fd2fbba2a202f072a46
Joshua_zr <awaken.zr@gmail.com>
2016-12-04 16:18:53 +0000

    test

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => {
-    res.send('timestamp-api');
+app.get('/:date', (req, res) => {
+    var str = req.params.date;
+    res.send(str);
 })
 
 app.listen(process.env.PORT || 5000);
